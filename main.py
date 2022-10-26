1ba9c9be86b3ad68a2b3090e0df09032ad209f10
no name <62671534+byronpiz@users.noreply.github.com>
2022-10-26 14:19:22 +0000

    se navega por wikipedia se navega por cgr y busca contenido
    se llena formulario y se envia

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https:/google.com")
-
-
 ## Encontrar Elementos
 
 google_text = driver.find_element(By.CLASS_NAME, "MV3Tnb").text
@@ -19,16 +17,16 @@ print(google_text)
 
 input_box = driver.find_element(By.NAME, "q")
 
-input_box.send_keys("selenium")
+input_box.send_keys("byron")
 
 input_box.send_keys(Keys.ENTER)
-
 ## Pausa de 5 segundos
 
 import time
 
+
 print('esperaremos 5 segundos' )
-time.sleep(5)
+#time.sleep(5)
 
 ## Volvemos a la home page
 
@@ -36,7 +34,9 @@ home_link = driver.find_element(By.ID, "logo")
 
 home_link.click()
 
+
 ## Buscar
+
 
 input_box = driver.find_element(By.NAME, "q")
 
@@ -44,8 +44,36 @@ input_box.send_keys("selenium")
 
 input_box.send_keys(Keys.ENTER)
 
-link = driver.find_element(By.PARTIAL_LINK_TEXT, "Selenium")
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "dev")
 
 print(link.text)
 
 link.click()
+driver.get("https://es.wikipedia.org")
+time.sleep(5)
+
+driver.get("https:/www.contraloria.cl")
+
+
+
+cgr_cerrar = driver.find_element(By.CLASS_NAME, "fa-times-circle-o")
+cgr_cerrar.click()
+#print(cgr_text)
+
+
+input_box = driver.find_element(By.CLASS_NAME, "_1qG0")
+input_box.send_keys("cgr")
+input_box.click()
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+input_text = driver.find_element(By.ID, "my-text-id")
+input_text.send_keys("texto")
+input_pass = driver.find_element(By.NAME, "my-password")
+input_pass.send_keys("secreto")
+input_area = driver.find_element(By.NAME, "my-textarea")
+input_area.send_keys("texto super largo")
+input_area = driver.find_element(By.CLASS_NAME, "form-select")
+input_area.send_keys("2")
+
+btm_enviar = driver.find_element(By.CLASS_NAME, "btn-outline-primary")
+btm_enviar.click()
